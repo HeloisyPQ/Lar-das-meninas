4b7872cc34d0261ee58899d181f05e2dedd1284b
HeloisyPimenta <heloisy.pimenta1@gmail.com>
2021-01-04 02:21:14 -0400

    Botão funcional de adicionar campo para foto.

--- local/scripts/page-create-orphanage.js
+++ local/scripts/page-create-orphanage.js
@@ -1,62 +1,63 @@
 
-const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheeZoom: false,
-    zoomControl: false
-}
-// Variável contante const mymap, objeto L 
-// Acessa mymap
-//e setView([Latitude, Logintude], Zoom)
-//const mymap = L.map('mapid').setView([51.505, -0.09], 15);
+    // Variável contante const mymap, objeto L 
+    // Acessa mymap
+    //e setView([Latitude, Logintude], Zoom)
+    //const mymap = L.map('mapid').setView([51.505, -0.09], 15);
+
     // Create map. 
-    const map = L.map('mapid', options).setView([-8.757835, -63.872515], 13); 
+    const map = L.map('mapid').setView([-8.757835, -63.872515], 13); 
 
     // Create and add tileLayer. 
-L.tileLayer( // Cria o tileLayer, a primeira camada que vai recebr o mapa. 
+    L.tileLayer( // Cria o tileLayer, a primeira camada que vai recebr o mapa. 
 
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // Argumento um (1): String. 
      
-).addTo(map); // Adiciona para o mapa (map). 
+    ).addTo(map); // Adiciona para o mapa (map). 
 
     // Create icon. 
-    
+
     const icon = L.icon({ // Objeto e propiedades. 
         iconUrl : "./local/images/map-marker.svg",
         ionSize: [58, 68], // [Largura, altura]
         iconAnchor: [29, 68], // Onde vai estar ancorado. 
-        popupAnchor: [170, 2] // Quando abrir, o popup vai ficar ao lado. 
     })
 
-   
-    // Create and add marker. 
+let marker;
 
-L.marker([-8.757835, -63.872515], {icon: icon}). // Cria uma marcação na posição [Posição1, Posição2]. 
-addTo(map) // Adiciona ao mapa. 
-   
-    // Image gallery 
+    // Creat and add marker
+    map.on('click', (event) => {
+        // console.log(event);
+        const lat = event.latlng.lat;
+        const lng = event.latlng.lng;
 
-    function selectImage(event) {
-        const button = event.currentTarget;
+        document.querySelector('[name=lat]').value = lat;
+        document.querySelector('[name=lng]').value = lng;
 
-        // Remosver todas as classes .active 
-        const buttons = document.querySelectorAll(".images button");
-        console.log(buttons);
-        console.log(buttons.children);
-        buttons.forEach(removeActiveClass)
+    //remove icon
+    marker && map.removeLayer(marker)
 
-        function removeActiveClass(button){
-            button.classList.remove("active")
-        }
+    // add icon layer
+    marker = L.marker([lat, lng], {icon})
+    .addTo(map)
+})
 
-        // Selecionar a imagem clicada 
-        const image = button.children[0];
-        const imageContainer = document.querySelector(".orphanage-details > img")
+// Adicionar o campo de foto
+function addPhotoField() {
+    console.log(' Teste. ');
+    // Pegar o container de fotos #images
+    const container = document.querySelector('#images')
+    // Pegar o container para duplicar .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    // Realizar o clone da última imagem adicionada. 
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    // Verificar se o campo está vazio, se sim, não adicionar ao container de imagens 
+    const input = newFieldContainer.children[0]
 
-        // Atualizar o container de imagem 
-        imageContainer.src = image.src;
-
-        // Adicionar a classe .active para este botão 
-        button.classList.add("active")
+    if(input.value == "") {
+        return;
     }
+    // Limpar o campo antes de adicionar ao container de imagens 
+    input.value = "";
+    // Adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
